fb765d051c4cdb19637ec61bdfe298c7efe3f51d
Pasha <pavik.varganov@gmail.com>
2024-10-31 15:38:48 +0200

    realise city app

---
+++ city/crud.py
@@ -0,0 +1,33 @@
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from city import models, schemas
+
+
+async def get_all_cities(db: AsyncSession):
+    query = select(models.City)
+    notes_list = await db.execute(query)
+    return [note[0] for note in notes_list.fetchall()]
+
+
+async def create_city(db: AsyncSession, city: schemas.CityCreate):
+    query = insert(models.City).values(
+        name=city.name,
+        additional_info=city.additional_info,
+    )
+    result = await db.execute(query)
+    await db.commit()
+    resp = {**city.model_dump(), "id": result.lastrowid}
+    return resp
+
+
+async def delete_city(db: AsyncSession, city_id: int) -> bool:
+    result = await db.execute(
+        select(models.City).filter(models.City.id == city_id)
+    )
+    db_city = result.scalar_one_or_none()
+    if db_city:
+        await db.delete(db_city)
+        await db.commit()
+        return True
+    return False
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from city import router as city_router
 
 app = FastAPI()
 
-# app.include_router(city_router.router)
+app.include_router(city_router.router)
 
 
 @app.get("/")
